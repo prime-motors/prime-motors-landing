b7fadbd0c2cb08ff9f60bb854571102671aedfb5
Stanislav Spatari <spatari.stanislav@gmail.com>
2026-02-19 22:37:22 +0200

    fix: facebook pixel using vite plugin radar
    Related: Ticket-11

--- src/main.tsx
+++ src/main.tsx
@@ -4,31 +4,6 @@ import './index.css'
 import './i18n'
 import App from './App'
 
-// Facebook Pixel
-const fbPixelId = import.meta.env.VITE_FACEBOOK_PIXEL_ID
-if (fbPixelId) {
-  /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-expressions, prefer-spread, prefer-rest-params */
-  ;(function (f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
-    if (f.fbq) return
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    }
-    if (!f._fbq) f._fbq = n
-    n.push = n
-    n.loaded = !0
-    n.version = '2.0'
-    n.queue = []
-    t = b.createElement(e)
-    t.async = !0
-    t.src = v
-    s = b.getElementsByTagName(e)[0]
-    s.parentNode.insertBefore(t, s)
-  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
-  ;(window as any).fbq('init', fbPixelId)
-  ;(window as any).fbq('track', 'PageView')
-  /* eslint-enable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-expressions, prefer-spread, prefer-rest-params */
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
